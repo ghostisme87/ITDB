f80e46f9f41fe34f7fafde5a2069c0cdc1ee2db7
ghostisme87 <vkostyannikov@gmail.com>
2016-02-08 17:12:15 +0200

    UID EditBox Added

--- ITDBServer/ITDBServer/Resource.h
+++ ITDBServer/ITDBServer/Resource.h
@@ -7,8 +7,9 @@
 #define IDR_MAINFRAME                   128
 #define IDR_ITDBServerTYPE              130
 #define IDD_DBCONNECT                   310
-#define IDC_LIST1                       1000
 #define IDC_LSDSN                       1000
+#define IDC_EDIT1                       1001
+#define IDC_EUID                        1001
 
 // Next default values for new objects
 // 
@@ -16,7 +17,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        312
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_CONTROL_VALUE         1002
 #define _APS_NEXT_SYMED_VALUE           310
 #endif
 #endif
